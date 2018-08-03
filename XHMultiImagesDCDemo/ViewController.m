88c1a6761db604dd576ca37e22489b53f0bd4fd5
fengxiaohui <fengxiaohui@xinguangnet.com>
2018-08-03 16:16:16 +0800

    update

--- XHMultiImagesDCDemo/ViewController.m
+++ XHMultiImagesDCDemo/ViewController.m
@@ -7,12 +7,10 @@
 //
 
 #import "ViewController.h"
-//#import "DCTool.h"
 #import "SDWebImageManager.h"
 #import "MBProgressHUD.h"
 
 #define BaiduLogoURL @"https://www.baidu.com/img/bd_logo1.png?where=super"
-
 #define MineImageURLCommonPart @"https://raw.githubusercontent.com/wiki/xiaohuiCoding/blogImages/Demo/resource_20180611_"
 
 #define CommonWidth 135
@@ -20,13 +18,7 @@
 
 @interface ViewController ()
 
-@property (nonatomic, strong) UIImage *image1;
-@property (nonatomic, strong) UIImage *image2;
-@property (nonatomic, strong) UIImage *image3;
-@property (nonatomic, strong) UIImage *image4;
-
 @property (nonatomic, strong) NSMutableArray *imageArray;
-@property (nonatomic, strong) UIImage *resultImage;
 @property (nonatomic, strong) UIImageView *resultImageView;
 @property (nonatomic, strong) UIButton *actionButton;
 
@@ -66,6 +58,7 @@
 {
     /*****实现分组并发网络请求*****/
     
+    //在主线程中加载HUD
     dispatch_async(dispatch_get_main_queue(), ^{
         [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     });
@@ -79,8 +72,8 @@
         
         NSLog(@"异步下载1");
 //        NSURL*url = [NSURL URLWithString:BaiduLogoURL];
-        NSURL*url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%d.png", MineImageURLCommonPart, 0]];
-        NSData*data = [NSData dataWithContentsOfURL:url];
+        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%d.png", MineImageURLCommonPart, 0]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
         UIImage *image = [UIImage imageWithData:data];
         if (image) {
             [self.imageArray addObject:image];
@@ -90,8 +83,8 @@
     dispatch_group_async(group, queue, ^{
         NSLog(@"异步下载2");
 //        NSURL*url = [NSURL URLWithString:BaiduLogoURL];
-        NSURL*url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%d.png", MineImageURLCommonPart, 1]];
-        NSData*data = [NSData dataWithContentsOfURL:url];
+        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%d.png", MineImageURLCommonPart, 1]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
         UIImage *image = [UIImage imageWithData:data];
         if (image) {
             [self.imageArray addObject:image];
@@ -101,8 +94,8 @@
     dispatch_group_async(group, queue, ^{
         NSLog(@"异步下载3");
 //        NSURL*url = [NSURL URLWithString:BaiduLogoURL];
-        NSURL*url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%d.png", MineImageURLCommonPart, 2]];
-        NSData*data = [NSData dataWithContentsOfURL:url];
+        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%d.png", MineImageURLCommonPart, 2]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
         UIImage *image = [UIImage imageWithData:data];
         if (image) {
             [self.imageArray addObject:image];
@@ -112,8 +105,8 @@
     dispatch_group_async(group, queue, ^{
         NSLog(@"异步下载4");
 //        NSURL*url = [NSURL URLWithString:BaiduLogoURL];
-        NSURL*url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%d.png", MineImageURLCommonPart, 3]];
-        NSData*data = [NSData dataWithContentsOfURL:url];
+        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%d.png", MineImageURLCommonPart, 3]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
         UIImage *image = [UIImage imageWithData:data];
         if (image) {
             [self.imageArray addObject:image];
@@ -132,11 +125,11 @@
             [self.imageArray[i] drawInRect:CGRectMake(0, CommonHeight*i, CommonWidth, CommonHeight)];
         }
         //根据图形上下文拿到图片
-        UIImage*image =UIGraphicsGetImageFromCurrentImageContext();
+        UIImage *image =UIGraphicsGetImageFromCurrentImageContext();
         //关闭上下文
         UIGraphicsEndImageContext();
         
-        //主线程刷新UI
+        //在主线程中刷新UI
         dispatch_async(dispatch_get_main_queue(), ^{
             
             [MBProgressHUD hideHUDForView:self.view animated:YES];
@@ -147,14 +140,6 @@
         });
         
     });
-    
-    //    dispatch_barrier_sync(queue, ^{
-    //        NSLog(@"下载结束，开始组合，显示要回到主线程");
-    //
-    //        dispatch_async(dispatch_get_main_queue(), ^{
-    //            self.imgView.image = self.resultImage;
-    //        });
-    //    });
 }
 
 @end
